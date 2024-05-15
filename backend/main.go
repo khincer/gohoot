e02aeb0d87ad25ee92016468d2516a872c2ee6b8
Kirk <kirk.incer@ahinko.com>
2024-05-15 17:31:20 -0600

    feat: basic server initialization and routing using local MongoDB driver

---
+++ backend/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var quizCollection *mongo.Collection
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", index)
+	app.Get("/api/quizzes", getQuizzes)
+
+	log.Fatal(app.Listen(":3000"))
+}
+
+func index(c *fiber.Ctx) error {
+	return c.SendString("Hello, World!")
+}
+
+func setupDb() {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx,
+		options.Client().ApplyURI("mongodb://localhost:27017/"))
+	if err != nil {
+		panic(err)
+	}
+
+	quizCollection = client.Database("gohoot").Collection("quizzes")
+}
+
+func getQuizzes(c *fiber.Ctx) error {
+	setupDb()
+	cursor, err := quizCollection.Find(context.Background(), bson.M{})
+
+	if err != nil {
+		panic(err)
+	}
+
+	quizzes := []map[string]any{}
+	err = cursor.All(context.Background(), &quizzes)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return c.JSON(quizzes)
+}
